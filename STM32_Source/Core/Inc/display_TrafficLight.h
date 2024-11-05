a487af3d4c788febea4ba3a902ef6e35c5d73452
BINHBO\doand <binh.doanduc04@hcmut.edu.vn>
2024-11-06 00:57:02 +0700

    Init for Lab3_Exercise7-8-9

--- STM32_Source/Core/Inc/display_TrafficLight.h
+++ STM32_Source/Core/Inc/display_TrafficLight.h
@@ -8,6 +8,20 @@
 #ifndef INC_DISPLAY_TRAFFICLIGHT_H_
 #define INC_DISPLAY_TRAFFICLIGHT_H_
 
+#include "main.h"
 
+#define TRAFFIC_NUMS 		2
+#define TRAFFIC_SEC_DUR 	1000
+#define TRAFFIC_BLINK_DUR 	500
+
+enum Traffic_State {Traffic_Off, Traffic_Red, Traffic_Yellow, Traffic_Green};
+
+extern int trafficRedDuration;
+extern int trafficYellowDuration;
+extern int trafficGreenDuration;
+extern enum Traffic_State trafficState[TRAFFIC_NUMS];
+
+void trafficInitState(void);
+void trafficReState(int idx, enum Traffic_State state);
 
 #endif /* INC_DISPLAY_TRAFFICLIGHT_H_ */
--- STM32_Source/Core/Src/display_TrafficLight.c
+++ STM32_Source/Core/Src/display_TrafficLight.c
@@ -5,4 +5,54 @@
  *      Author: doand
  */
 
+#include "display_TrafficLight.h"
 
+int trafficRedDuration = 0;
+int trafficYellowDuration = 0;
+int trafficGreenDuration = 0;
+enum Traffic_State trafficState[TRAFFIC_NUMS] = {0, 0};
+
+static uint16_t trafficRedPins[TRAFFIC_NUMBER] = {RED1_Pin_Pin, Red2_Pin};
+static uint16_t trafficYellowPins[TRAFFIC_NUMBER] = {Yellow1_Pin, Yellow2_Pin};
+static uint16_t trafficGreenPins[TRAFFIC_NUMBER] = {Green1_Pin, Green2_Pin};
+
+void trafficInitState(void) {
+	trafficRedDuration = 5;
+	trafficGreenDuration = 3;
+	trafficYellowDuration = 2;
+	trafficReState(0, Traffic_Off);
+	trafficReState(1, Traffic_Off);
+}
+
+void trafficReState(int idx, enum Traffic_State state) {
+	if (idx < 0 || idx >= TRAFFIC_NUMS) return;
+
+	switch (state) {
+	case TRAFFIC_OFF:
+		HAL_GPIO_WritePin(GPIOA, trafficRedPins[index], GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(GPIOA, trafficYellowPins[index], GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(GPIOA, trafficGreenPins[index], GPIO_PIN_RESET);
+		trafficState[idx] = Traffic_Off;
+		break;
+	case TRAFFIC_RED:
+		HAL_GPIO_WritePin(GPIOA, trafficRedPins[index], GPIO_PIN_SET);
+		HAL_GPIO_WritePin(GPIOA, trafficYelllowPins[index], GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(GPIOA, trafficGreenPins[index], GPIO_PIN_RESET);
+		trafficState[idx] = Traffic_Red;
+		break;
+	case TRAFFIC_AMBER:
+		HAL_GPIO_WritePin(GPIOA, trafficRedPins[index], GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(GPIOA, trafficYellowPins[index], GPIO_PIN_SET);
+		HAL_GPIO_WritePin(GPIOA, trafficGreenPins[index], GPIO_PIN_RESET);
+		trafficState[idx] = Traffic_Yellow;
+		break;
+	case TRAFFIC_GREEN:
+		HAL_GPIO_WritePin(GPIOA, trafficRedPins[index], GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(GPIOA, trafficYellowPins[index], GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(GPIOA, trafficGreenPins[index], GPIO_PIN_SET);
+		trafficState[idx] = Traffic_Green;
+		break;
+	default:
+		break;
+	}
+}
